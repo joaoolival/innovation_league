b2459a160d5d170a00e9769a741c427862aa310b
joaoolival <joaoljolival@gmail.com>
2020-12-03 01:40:38 +0000

    refactor with fabio home blade and src

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,145 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home Innovation League</title>
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/bootstrap/css/style.css">
     </head>
     <body class="antialiased">
-        @php
-        
-        if(isset($round_16[0])) 
-        {
-            dd('nao');
-        }
-        else {
-            dd('sim');
-        }
-        
-        //@foreach ($leagues as $league)
+        <div class="bgimage">
             
-        //@endforeach
-        @endphp
+            <div class="row">
+
+                <!-- GRUPO A -->
+                <div class="column">
+                    @php
+                        //dd($leagues)
+                        $pos=0;
+                    @endphp
+                    <div class="bgtitulogrupo">
+                        Grupo A
+                    </div>
+                    <div class="bggrupo">
+                        <table>
+                            <tr style="border-bottom: none;">
+                                <th style="text-align: left;">POS</th>
+                                <th style="text-align: left;">EQUIPA</th>
+                                <th>J</th>
+                                <th>W</th>
+                                <th>D</th>
+                                <th>L</th>
+                                <th>PTS</th>
+                            </tr>
+                            @foreach($leagues as $league)
+                                @if ($league[0]->Group_name == 'A')
+                                    @foreach($league as $team)
+                                    @php
+                                        //dd($team);
+                                        $pos++;
+                                    @endphp
+                                        <tr>
+                                        <td style="text-align: left">{{$pos}}</td>
+                                            <td style="text-align: left">{{$team->Name}}</td>
+                                            <td>{{$team->Matches}}</td>
+                                            <td>{{$team->Won}}</td>
+                                            <td>{{$team->Drawn}}</td>
+                                            <td>{{$team->Lost}}</td>
+                                            <td>{{$team->Points}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+                        <!-- FIM GRUPO A -->  
+                <br>
+    
+                <!-- GRUPO B -->
+                <div class="column">
+                    @php
+                        //dd($leagues)
+                        $pos=0;
+                    @endphp
+                    <div class="bgtitulogrupo">
+                        Grupo B
+                    </div>
+                    <div class="bggrupo">
+                        <table>
+                            <tr style="border-bottom: none;">
+                                <th style="text-align: left;">POS</th>
+                                <th style="text-align: left;">EQUIPA</th>
+                                <th>J</th>
+                                <th>W</th>
+                                <th>D</th>
+                                <th>L</th>
+                                <th>PTS</th>
+                            </tr>
+                            @foreach($leagues as $league)
+                                @if ($league[0]->Group_name == 'B')
+                                    @foreach($league as $team)
+                                    @php
+                                        $pos++;
+                                    @endphp
+                                        <tr>
+                                        <td style="text-align: left">{{$pos}}</td>
+                                            <td style="text-align: left">{{$team->Name}}</td>
+                                            <td>{{$team->Matches}}</td>
+                                            <td>{{$team->Won}}</td>
+                                            <td>{{$team->Drawn}}</td>
+                                            <td>{{$team->Lost}}</td>
+                                            <td>{{$team->Points}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+                        <!-- FIM GRUPO B -->
+                <br>
+                <!-- GRUPO C -->
+                <div class="column">
+                    @php
+                        //dd($leagues)
+                        $pos=0;
+                    @endphp
+                    <div class="bgtitulogrupo">
+                        Grupo C
+                    </div>
+                    <div class="bggrupo">
+                        <table>
+                            <tr style="border-bottom: none;">
+                                <th style="text-align: left;">POS</th>
+                                <th style="text-align: left;">EQUIPA</th>
+                                <th>J</th>
+                                <th>W</th>
+                                <th>D</th>
+                                <th>L</th>
+                                <th>PTS</th>
+                            </tr>
+                            @foreach($leagues as $league)
+                                @if ($league[0]->Group_name == 'C')
+                                    @foreach($league as $team)
+                                    @php
+                                        $pos++;
+                                    @endphp
+                                        <tr>
+                                        <td style="text-align: left">{{$pos}}</td>
+                                            <td style="text-align: left">{{$team->Name}}</td>
+                                            <td>{{$team->Matches}}</td>
+                                            <td>{{$team->Won}}</td>
+                                            <td>{{$team->Drawn}}</td>
+                                            <td>{{$team->Lost}}</td>
+                                            <td>{{$team->Points}}</td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+                        <!-- FIM GRUPO c     -->
+            </div><!--row-->
+            
+        </div>
     </body>
 </html>
